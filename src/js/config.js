0cf3a0a63a4b19ac804702ff7d678e77098c3199
SubajananiSwaminathan <subajanani@gmail.com>
2024-06-24 12:34:36 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'd544ab7c-cc02-4b2e-8ad3-530c84d1205d';
+export const MODAL_CLOSE_SEC = 2.5;
